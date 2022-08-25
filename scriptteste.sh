03b8be9fa0923d9afba1a7da603b805d9efaba1a
julianoSchaeffer <111443229+julianoSchaeffer@users.noreply.github.com>
2022-08-25 19:11:18 -0300

    Delete scriptteste.sh

--- scriptteste.sh
+++
@@ -1,9 +0,0 @@
-sudo apt update
-sudo apt upgrade -y
-sudo apt install code -y
-sudo apt install virtual box -y
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub && sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main"
->> /etc/apt/sources.list.d/google.list'
-sudo apt update
-sudo apt install google-chrome-stable
