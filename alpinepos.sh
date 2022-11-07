6f925bfff29055107ad763c2a5a45cde9c6a2085
julianoSchaeffer <111443229+julianoSchaeffer@users.noreply.github.com>
2022-11-07 19:15:00 -0300

    Add files via upload

---
+++ alpinepos.sh
@@ -0,0 +1,2 @@
+setup-xorg-base gnome gnome-terminal gnome-dektop mesa-dri-swrast dbus-x11 sudo gvfs-fuse gvfs-smb fuse-openrc thunar-volman udisks2 mysql firefox  
+rc-update add udev && rc-update add gdm && rc-update add udev-trigger && rc-update add udev-settle
