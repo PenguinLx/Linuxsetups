2f59837462b6eaedd079ba5e257c395edc5c123c
julianoSchaeffer <111443229+julianoSchaeffer@users.noreply.github.com>
2022-11-07 19:18:41 -0300

    Update alpinepos.sh

--- alpinepos.sh
+++ alpinepos.sh
@@ -1,2 +1,2 @@
-setup-xorg-base gnome gnome-terminal gnome-dektop mesa-dri-swrast dbus-x11 sudo gvfs-fuse gvfs-smb fuse-openrc thunar-volman udisks2 mysql firefox  
+setup-xorg-base gnome gnome-terminal gnome-dektop mesa-dri-swrast dbus-x11 sudo  mysql firefox  
 rc-update add udev && rc-update add gdm && rc-update add udev-trigger && rc-update add udev-settle
